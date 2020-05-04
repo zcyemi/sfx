fb75f2fdc3c82c2666af5455e27821af1ab6494d
yemi <zcyemi@gmail.com>
2020-05-04 14:27:47 +0800

    release 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,8 @@
 
 ---
 
+- 0.1.1:
+    + Fix symbol export.
+
 - 0.1.0:
-    + First version
+    + First version.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './SFXCompilationCtx';
 export * from './SFXFileResolver';
 export * from './SFXSource';
+export * from './SFXSourceVisotor';
 export * from './SFXFileWatcher';
 export * from './SFXConfig';
+export * from './Utility';
